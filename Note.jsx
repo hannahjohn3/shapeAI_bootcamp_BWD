872c79e1640a309ded65a971ce093ad242be9c36
hannahjohn3 <85452367+hannahjohn3@users.noreply.github.com>
2021-06-06 22:36:50 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amazing Bootcamp on Javascript and React.js.It was a 7 day
+        Bootcamp taken up by Mr.Shaurya Sinha.The Bootcamp covered everything
+        from scratch including javascript React.js,HTML
+      </p>
+    </div>
+  );
+}
+export default Note;
---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./Components/App";
+import "../public/styles.css";
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
